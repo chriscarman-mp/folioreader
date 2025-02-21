e22acaf6bc6b7540a7abab341f375bd5709bb1bc
Chris Carman <chris@madepurple.com>
2025-02-21 15:28:57 +0100

    Disable opening external urls

--- folioreader/src/main/java/com/folioreader/ui/fragment/FolioPageFragment.kt
+++ folioreader/src/main/java/com/folioreader/ui/fragment/FolioPageFragment.kt
@@ -567,7 +567,7 @@ class FolioPageFragment() : Fragment(),
             if (urlOfEpub != true) {
                 // Otherwise, give the default behavior (open in browser)
                 val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                startActivity(intent)
+                // startActivity(intent)
             }
 
             return true
